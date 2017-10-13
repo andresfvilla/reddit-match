93524d3e591c02aa917aa33dc64df0a7528e0fc4
Garrett May <gamay@us.ibm.com>
2017-10-13 14:36:13 -0400

    dev info

--- webapp/client/Footer/Footer.js
+++ webapp/client/Footer/Footer.js
@@ -3,6 +3,12 @@ import {Link} from 'react-router';
 import FooterStore from './FooterStore'
 import FooterActions from './FooterActions';
 
+var Andres = '../img/andres.jpeg';
+var Garrett = '../img/garrett.jpeg';
+var Logan = '../img/logan.jpeg';
+var Slack = '../img/slack.svg';
+var GitHub = '../img/github.png'
+
 class Footer extends React.Component {
   constructor(props) {
     super(props);
@@ -30,10 +36,24 @@ class Footer extends React.Component {
           <div className='row'>
             <div className='devs'>
               <h3 className='lead'><strong></strong></h3>
-              <span><a href="https://github.ibm.com/Andres-Villa">@andres.villa</a></span>
-              <span><a href="https://github.ibm.com/gamay">@gamay</a></span>
-              <span><a href="https://github.ibm.com/loganpatino10">@lopa</a></span>
-              <h3 className='lead'><strong></strong></h3>
+              <div className='image'>
+                <img className='devImage' src={Andres} />
+                <div>Andres Villa</div>
+                <a className='contactIconContainer' href="slack://user?team=T03EBGYRJ&id=W4ED6QUDB"><img className='contactIcon' src={Slack}/></a>
+                <a className='contactIconContainer' href="https://github.ibm.com/Andres-Villa"><img className='contactIcon' src={GitHub}/></a>
+              </div>
+              <div className='image'>
+                <img className='devImage' src={Garrett} />
+                <div>Garrett May</div>
+                <a className='contactIconContainer' href="slack://user?team=T03EBGYRJ&id=W48E9TFHN"><img className='contactIcon' src={Slack}/></a>
+                <a className='contactIconContainer' href="https://github.ibm.com/gamay"><img className='contactIcon' src={GitHub}/></a>
+              </div>
+              <div className='image'>
+                <img className='devImage' src={Logan} />
+                <div>Logan Patiño</div>
+                <a className='contactIconContainer' href="slack://user?team=T03EBGYRJ&id=W692BLT0D"><img className='contactIcon' src={Slack}/></a>
+                <a className='contactIconContainer' href="https://github.ibm.com/loganpatino10"><img className='contactIcon' src={GitHub}/></a>
+              </div>
             </div>
           </div>
         </div>
