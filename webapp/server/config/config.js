0499da4ddf2cc46cee1bc4a5dbf875e8a363f3f6
andres.villa@ibm.com <andres.villa@ibm.com>
2017-10-13 16:07:30 -0400

    get profile from PI

--- webapp/server/config/config.js
+++ webapp/server/config/config.js
@@ -11,9 +11,12 @@ module.exports = {
     redditClientSecret: process.env.redditClientSecret,
     redditUserAgent: process.env.redditUserAgent,
     redditRefreshToken: process.env.redditRefreshToken,
+    watsonUsername: process.env.watsonUsername,
+    watsonPassword: process.env.watsonPassword,
+    watsonVersion: process.env.watsonVersion,
     port: PORT,
     mongo: {
-        uri: process.env.MONGO_URL || 'mongodb://localhost/scnavis'
+        uri: process.env.MONGO_URL || 'mongodb://localhost/'
     },
     imageDest: "server/public/img/profiles/",
     url: URL
