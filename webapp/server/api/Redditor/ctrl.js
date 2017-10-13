9abf274e16760fa0834feb6b8c695c17c19cd522
andres.villa@ibm.com <andres.villa@ibm.com>
2017-10-13 12:50:11 -0400

    removed multer usage

--- webapp/server/api/Redditor/ctrl.js
+++ webapp/server/api/Redditor/ctrl.js
@@ -1,7 +1,6 @@
 module.exports = function(app, passport) {
 
   //var upload = multer({ storage: storage, limits: {fileSize: 600000}})
-  var upload = multer({ storage: storage})
 
   app.post('/api/getComments/:user', function (req, res, next) {
       var filePath = app.config.imageDest + req.file.filename;
