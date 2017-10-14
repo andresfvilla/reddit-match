98bc6b2d611e282ea0b84b17f95fd48aee35dd05
Garrett May <gamay@us.ibm.com>
2017-10-13 22:42:59 -0400

    tidy UI

--- webapp/client/ResponseBlock/ResponseBlock.js
+++ webapp/client/ResponseBlock/ResponseBlock.js
@@ -19,31 +19,42 @@ class ResponseBlock extends React.Component {
         </div>
         {
           (this.props.response.subreddits) ?
-            this.props.response.subreddits.map(subreddit => 
-            <div className='subreddit'>
-              <a href={`https://www.reddit.com/r/` + subreddit}>{`/r/` + subreddit}</a>
-            </div>)
+            <div>
+              <p>Matching subreddits:</p>
+              <div className='subreddits'>
+                {
+                  this.props.response.subreddits.map(subreddit => 
+                  <div className='subreddit'>
+                    <a href={`https://www.reddit.com/r/` + subreddit}>{`/r/` + subreddit}</a>
+                  </div>)
+                }
+              </div>
+            </div>
           :
             null
         }
-        Analyzed comments:
-        <div className='comments'>
-          {
-            (this.props.response.comments) ?
-              this.props.response.comments
-            :
-              null
-          }
-        </div>
-        Personality profile:
-        <div className='personalityProfile'>
-          {
-            (this.props.response.personality_profile) ?
-              JSON.stringify(this.props.response.personality_profile,null,2)
-            :
-              null
-          }
-        </div>
+        {
+          (this.props.response.comments) ?
+            <div>
+              <p>Analyzed text:</p>
+              <div className='comments'>
+                {this.props.response.comments}              
+              </div>
+            </div>
+          :
+            null
+        }       
+        {
+          (this.props.response.personality_profile) ?
+            <div>
+              <p>Redditor personality profile:</p>
+              <div className='personalityProfile'>
+                {JSON.stringify(this.props.response.personality_profile,null,2)}
+              </div>
+            </div>
+          :
+            null
+        }
       </div>
     );
   }
